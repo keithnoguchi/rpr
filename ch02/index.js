b97bba97fef26ab8722ac53a67c6e89a09b7bcc3
Keith Noguchi <hack@noguchi.us>
2022-12-28 19:33:31 -0800

    ch02: Fix the move in index.js

--- ch02/index.js
+++ ch02/index.js
@@ -25,11 +25,17 @@ fetch('./checkers.wasm').then(response =>
   instance.exports.move(0, 3, 1, 4); // W
   instance.exports.move(2, 3, 1, 2); // B
   instance.exports.move(1, 4, 0, 5); // W
-  instance.exports.move(1, 2, 0, 0); // B - this will be crowned
+  instance.exports.move(2, 5, 3, 4); // B
   instance.exports.move(3, 2, 4, 3); // W
-  instance.exports.move(0, 0, 0, 2); // W
+  instance.exports.move(4, 5, 5, 4); // B
+  instance.exports.move(2, 1, 3, 2); // W
+  instance.exports.move(1, 2, 2, 1); // B
+  instance.exports.move(0, 1, 1, 2); // W
+  instance.exports.move(5, 4, 6, 3); // B
+  instance.exports.move(1, 0, 0, 1); // W
+  instance.exports.move(2, 1, 1, 0); // B - Crowned
 
-  let res = instance.exports.getPiece(0, 2);
+  let res = instance.exports.getPiece(1, 0);
   document.getElementById("container").innerText = res;
   console.log("At end, turn owner is " + instance.exports.getTurnOwner());
 }).catch(console.error);
